8c2c0e8dd1bb1f66248e86685a70e91d85263c55
Carlos Alten <carlos.alten@usm.cl>
2025-04-17 11:47:19 -0400

    Intro a JS 301

---
+++ 301/clase01/js/01_variables.js
@@ -0,0 +1,14 @@
+//constante
+const foo = "Esta es una constante"
+console.log(foo)
+console.log(typeof foo)
+
+//variable
+let bar = "Esto es variable"
+console.log(bar)
+
+let edad
+console.log(typeof edad)
+
+let otraVariable = null
+console.log(typeof otraVariable)
